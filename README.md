c69eea8be1bd1cd61b4a6ecd676b918388a93071
eduardomizael <eduardo.mizael@gmail.com>
2025-08-21 13:12:29 -0400

    Update README with synchronization instructions and refine dependency lockfile configurations.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### Escolha um grupo ao sincronizar:
+- uv sync -E gpu -> instala wheels com CUDA (Windows/Linux)
+- uv sync -E cpu -> instala wheels CPU-only (Windows/Linux/macOS)
---
+++ detector_v1.py
@@ -0,0 +1,65 @@
+from ultralytics import YOLO
+import cv2
+from collections import defaultdict
+import numpy as np
+
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r'videos\video04.mp4')
+
+# TODO ver como isso funciona
+# Function to loop the video
+def loop_video(cap):
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+# Add this line inside the while loop after reading the frame
+loop_video(cap)
+model = YOLO(r"runs\detect\train6\weights\best.pt")
+
+track_history = defaultdict(lambda: [])
+seguir = True
+deixar_rastro = True
+
+while True:
+    success, img = cap.read()
+
+    if success:
+        if seguir:
+            results = model.track(img, persist=True)
+        else:
+            results = model(img)
+
+        # Process results list
+        for result in results:
+            # Visualize the results on the frame
+            img = result.plot()
+
+            if seguir and deixar_rastro:
+                try:
+                    # Get the boxes and track IDs
+                    boxes = result.boxes.xywh.cpu()
+                    track_ids = result.boxes.id.int().cpu().tolist()
+
+                    # Plot the tracks
+                    for box, track_id in zip(boxes, track_ids):
+                        x, y, w, h = box
+                        track = track_history[track_id]
+                        track.append((float(x), float(y)))  # x, y center point
+                        if len(track) > 30:  # retain 90 tracks for 90 frames
+                            track.pop(0)
+
+                        # Draw the tracking lines
+                        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                        cv2.polylines(img, [points], isClosed=False, color=(230, 0, 0), thickness=5)
+                except:
+                    pass
+
+        cv2.imshow("Tela", img)
+
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+print("desligando")
